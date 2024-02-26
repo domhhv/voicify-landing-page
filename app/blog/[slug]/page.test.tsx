f80dfe2ac3e4b1385ae2f2c1b090f93e44bb5184
Dominik Hryshaiev <domhryshaiev@gmail.com>
2024-02-26 09:51:36 +0200

    feat(lint): add prettier, eslint and husky

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -1,10 +1,11 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import Page from "./page";
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
 
-test("App Router: Works with dynamic route segments", () => {
-  render(<Page params={{ slug: "Test" }} />);
+import Page from './page';
+
+test('App Router: Works with dynamic route segments', () => {
+  render(<Page params={{ slug: 'Test' }} />);
   expect(
-    screen.getByRole("heading", { level: 1, name: "Slug: Test" }),
+    screen.getByRole('heading', { level: 1, name: 'Slug: Test' })
   ).toBeDefined();
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: "App Router",
+  title: 'App Router',
 };
 
 export default function Page() {
